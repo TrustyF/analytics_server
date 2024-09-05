29f5fd2cd45fe414f8b1336eb3a9760ffec7bade
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-09-04 20:31:34 -0700

    Obfuscated api key

--- constants.py
+++ constants.py
@@ -7,5 +7,5 @@ load_dotenv(os.path.join(MAIN_DIR, '.env'))
 FLASK_SECRET = os.getenv('FLASK_SECRET')
 DB_USERNAME = os.getenv('MYSQL_DATABASE_USERNAME')
 DB_PASSWORD = os.getenv('MYSQL_DATABASE_PASSWORD')
+GEO_API = os.getenv('GEO_API')
 DB_NAME = 'TrustyFox$firebase_events'
-GEO_API = '2a96f8a25db2469b939c8f836cc9eed9'
